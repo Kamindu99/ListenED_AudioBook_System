6bf01593904bfe10d648bc88ed20f6089ebfaaf2
kamindu.g <kamindu.g@informaticsint.com>
2023-10-29 15:53:54 +0530

    spacy-legacy

--- django_backend/sinhalaNLP/views.py
+++ django_backend/sinhalaNLP/views.py
@@ -27,6 +27,8 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 
+nltk.download('averaged_perceptron_tagger')
+
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
